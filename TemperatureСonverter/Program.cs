809a6c568ef467a45f030d3eba94c9816d7ec0fd
ENDISit <mr.ilia08@mail.ru>
2024-06-06 23:55:26 +0300

    Готовая задача №1

--- TemperatureСonverter/Program.cs
+++ TemperatureСonverter/Program.cs
@@ -1,15 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace TemperatureСonverter
+class TemperatureConverter
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-        }
+        Console.WriteLine("Введите температуру в градусах Цельсия:");
+        double celsius = double.Parse(Console.ReadLine());
+        double fahrenheit = (celsius * 9 / 5) + 32;
+        Console.WriteLine($"Температура в градусах Фаренгейта: {fahrenheit}");
+
+        Console.WriteLine("Введите температуру в градусах Фаренгейта:");
+        fahrenheit = double.Parse(Console.ReadLine());
+        celsius = (fahrenheit - 32) * 5 / 9;
+        Console.WriteLine($"Температура в градусах Цельсия: {celsius}");
+
+        Console.WriteLine("Нажмите любую клавишу для выхода...");
+        Console.ReadLine();
     }
 }
